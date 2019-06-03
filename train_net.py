227fa72e388d0779c20beb54dd5e3d7987e5465e
Van-Tien, Hoang <van-tien.hoang@outlook.com>
2019-06-03 19:23:36 -0400

    clean

--- train_net.py
+++ train_net.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import os
 
 from datetime import datetime
-import time 
+import time
 
 import convert_to_tfrecords
 from alexnet import *
@@ -10,13 +10,13 @@ from alexnet import *
 BATCH_SIZE = [200, 56]
 EPOCHS = 40
 NUM_CLASSES = 31
-NUM_STEPS = 14 * EPOCHS 
+NUM_STEPS = 14 * EPOCHS
 
 train_layers = ['fc8']
 keep_prob = 0.5
 
 def train_():
-  with tf.Graph().as_default():  
+  with tf.Graph().as_default():
     global_step = tf.train.get_or_create_global_step()
 
     # Get images and labels for Office-31.
@@ -25,24 +25,24 @@ def train_():
     with tf.device('/cpu:0'):
       source_images, source_labels = inputs(True)
       target_images, target_labels = inputs(False)
-    
+
     _lambda = tf.placeholder(tf.float32)
-    
+
     # Initialize model
     source_model = AlexNet(source_images, keep_prob, NUM_CLASSES, train_layers, 'source/')
     target_model = AlexNet(target_images, keep_prob, NUM_CLASSES, train_layers, 'target/')
 
     # Link variable to model output
     source_score = source_model.fc8
-    target_score = target_model.fc8  
-    
+    target_score = target_model.fc8
+
     # Op for calculating the loss
     classification_loss, coral_loss, loss_ = loss(source_score, target_score, source_labels, _lambda)
-  
+
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
     train_op = train(loss_, global_step)
-        
+
     # Merge all summaries together
     merged_summary = tf.summary.merge_all()
 
@@ -65,7 +65,7 @@ def train_():
       # Load the pretrained weights into the non-trainable layer
       source_model.load_initial_weights(sess)
       target_model.load_initial_weights(sess)
-    
+
       # Start the queue runners.
       coord = tf.train.Coordinator()
       try:
@@ -80,7 +80,7 @@ def train_():
           # Initialize iterator with the training dataset
           cl_loss, cr_loss, loss_value, _ = sess.run([classification_loss, coral_loss, loss_, train_op], feed_dict={_lambda: (int(step / 14) + 1) / EPOCHS})
           step += 1
-        
+
           if step % 10 == 0:
             current_time = time.time()
             duration = current_time - _start_time
@@ -93,7 +93,7 @@ def train_():
                         'sec/batch)')
             print (format_str % (datetime.now(), step, cl_loss, cr_loss, loss_value,
                                examples_per_sec, sec_per_batch))
-          
+
           if step % 100 == 0:
             dirname = os.path.dirname(__file__)
             dest_directory = os.path.join(dirname, 'office31_data/train')
@@ -102,11 +102,11 @@ def train_():
             save_path = saver.save(sess, checkpoint_name)
             print("{} Model checkpoint saved at {}".format(datetime.now(),
                                                            checkpoint_name))
-            
+
           summary = tf.Summary()
           summary.ParseFromString(sess.run(merged_summary, feed_dict={_lambda: (int(step / 14) + 1) / EPOCHS}))
-          writer.add_summary(summary, step)  
-        
+          writer.add_summary(summary, step)
+
         dirname = os.path.dirname(__file__)
         dest_directory = os.path.join(dirname, 'office31_data/train')
         checkpoint_name = os.path.join(dest_directory,
@@ -114,12 +114,12 @@ def train_():
         save_path = saver.save(sess, checkpoint_name)
         print("{} Model checkpoint saved at {}".format(datetime.now(),
                                                            checkpoint_name))
-              
+
       except Exception as e:  # pylint: disable=broad-except
         coord.request_stop(e)
 
       coord.request_stop()
-      coord.join(threads, stop_grace_period_secs=10)  
+      coord.join(threads, stop_grace_period_secs=10)
 
 
 def main(_):
@@ -134,3 +134,10 @@ def main(_):
 
 if __name__ == '__main__':
   tf.app.run(main=main)
+
+def newfunction(arg):
+    import numpy as np
+    import seaborn as sns
+    sns.pairplot(sns.load_dataset("iris"))
+    import matplotlib.pyplot as plt
+    plt.show()
